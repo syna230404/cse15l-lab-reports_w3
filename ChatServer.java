725e07e47687df91f1c91bf2b6184e03a5f04f57
syna230404 <156245600+syna230404@users.noreply.github.com>
2024-01-30 22:30:49 -0800

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -1,37 +1,42 @@
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URI;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-public class ChatServer {
+interface URLHandler {
+    String handleRequest(URI url);
+}
+
+class ChatServerHandler implements URLHandler {
     private static StringBuilder chatMessages = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create();
-        server.bind(null, 8080);
-        server.createContext("/add-message", new AddMessageHandler());
-        server.setExecutor(null);
-        server.start();
+    @Override
+    public String handleRequest(URI url) {
+        // Extract parameters from the request URI
+        String query = url.getQuery();
+        String[] params = query.split("&");
+        String user = params[1].substring(params[1].indexOf('=') + 1);
+        String message = params[0].substring(params[0].indexOf('=') + 1);
+
+        // Format the message and update chatMessages
+        String formattedMessage = user + ": " + message + "\n";
+        chatMessages.append(formattedMessage);
+
+        // Return the entire chatMessages
+        return chatMessages.toString();
     }
+}
 
-    static class AddMessageHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-            // Extract parameters from the request URI
-            String query = exchange.getRequestURI().getQuery();
-            String[] params = query.split("&");
-            String user = params[1].substring(params[1].indexOf('=') + 1);
-            String message = params[0].substring(params[0].indexOf('=') + 1);
+public class ChatServer {
+    public static void main(String[] args) throws IOException {
+        // Create an instance of ChatServerHandler
+        ChatServerHandler chatServerHandler = new ChatServerHandler();
 
-            // Format the message and update chatMessages
-            String formattedMessage = user + ": " + message + "\n";
-            chatMessages.append(formattedMessage);
-
-            // Send the response
-            String response = chatMessages.toString();
-            exchange.sendResponseHeaders(200, response.length());
-            exchange.getResponseBody().write(response.getBytes());
-            exchange.close();
-        }
+        // Start the server on port 8080 with the ChatServerHandler
+        Server.start(8080, chatServerHandler);
     }
 }
